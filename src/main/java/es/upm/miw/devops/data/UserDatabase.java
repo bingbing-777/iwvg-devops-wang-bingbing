fc7f7af205b3e14f8060c93f20c95d25d8e96cfb
bingbing-777 <bingbing.wang@alumnos.upm.es>
2026-09-24 18:01:06 +0200

    Add endpoint PUT /user/{id} #42

--- src/main/java/es/upm/miw/devops/data/UserDatabase.java
+++ src/main/java/es/upm/miw/devops/data/UserDatabase.java
@@ -115,4 +115,20 @@ public class UserDatabase {
             user.setActive(true);
         }
     }
+
+    public void updateUser(Long id, User updatedUser) {
+        User user = findById(id);
+
+        if (user != null) {
+            user.setFirstName(updatedUser.getFirstName());
+            user.setFamilyName(updatedUser.getFamilyName());
+            user.setEmail(updatedUser.getEmail());
+            user.setIdentity(updatedUser.getIdentity());
+            user.setAddress(updatedUser.getAddress());
+            user.setCity(updatedUser.getCity());
+            user.setProvince(updatedUser.getProvince());
+            user.setPostalCode(updatedUser.getPostalCode());
+            user.setActive(updatedUser.isActive());
+        }
+    }
 }
